d94551fd16f8743561c7fe1e7de25dbc5799d847
azezcodes <aziz@swatitech.com>
2024-06-07 10:24:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MyEditor from "./MyEditor";
+
+const App = () => {
+  return <MyEditor />;
+};
+
+export default App;
---
+++ src/MyEditor.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const MyEditor = () => {
+  const [value, setValue] = useState("");
+  const quillRef = useRef(null);
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["bold", "italic", "underline"],
+      ["code-block"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "list",
+    "bullet",
+    "bold",
+    "italic",
+    "underline",
+    "code-block",
+  ];
+
+  return (
+    <div>
+      <div className="my-editor">
+        <ReactQuill
+          ref={quillRef}
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          modules={modules}
+          formats={formats}
+        />
+      </div>
+      <div className="editor-content">
+        <h2>Editor Content:</h2>
+        <div dangerouslySetInnerHTML={{ __html: value }} />
+      </div>
+    </div>
+  );
+};
+
+export default MyEditor;
